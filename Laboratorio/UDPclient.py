4577e537fb6381f373e7ea6b0fd3a714dce8a447
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2018-05-01 09:41:52 +0200

    Update UDPclient.py

--- Laboratorio/UDPclient.py
+++ Laboratorio/UDPclient.py
@@ -1,14 +1,22 @@
 from socket import*
+
 serverName = '127.0.0.1'
 serverPort = 12345
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket = socket(AF_INET, SOCK_DGRAM) #ipv4 su UDP
+
 clientSocket.settimeout(5)
-message=raw_input('Input lowercase sentence:')
+
+message = raw_input('Input lowercase sentence:')
+
 try:
     clientSocket.sendto(message, (serverName, serverPort))
+    
     response, serverAddress = clientSocket.recvfrom(2048)
+    
     print response
 except error, v:
     print 'error'
+    
     print v
+    
 clientSocket.close()
