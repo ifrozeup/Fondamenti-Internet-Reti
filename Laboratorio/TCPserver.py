dbd419f025d8f377908ff010b7105282c266c31a
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2018-05-01 09:50:33 +0200

    Update TCPserver.py

--- Laboratorio/TCPserver.py
+++ Laboratorio/TCPserver.py
@@ -1,12 +1,18 @@
 from socket import*
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort)) # prepara alla connessione
-serverSocket.listen(1) #passive open, accetta solo una connessione (in base al parametro)
+
+serverSocket.listen(1) #passive open, accetta solo una connessione in coda 
 print ('The server is ready to receive')
+
 while 1:
     connectionSocket, clientAddr = serverSocket.accept()
+    
     message = connectionSocket.recv(1024)
+    
     modifiedMessage = message.upper()
     connectionSocket.send(modifiedMessage)
+    
     connectionSocket.close()
