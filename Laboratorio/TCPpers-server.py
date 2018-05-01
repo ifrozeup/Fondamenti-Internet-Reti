4d6cac9109ac91fd286a76deca88d7ce4f1aa15a
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2018-05-01 09:56:58 +0200

    Update TCPpers-server.py

--- Laboratorio/TCPpers-server.py
+++ Laboratorio/TCPpers-server.py
@@ -9,10 +9,14 @@ serverSocket.listen(2) #numero di client in coda
 while 1:
     print 'Server pronto'
     connectionSocket, clientAddr = serverSocket.accept()
+    
     while 1:
         message = connectionSocket.recv(1024)
+        
         if message == '.':
             break
+            
         modifiedMessage = message.upper()
         connectionSocket.send(modifiedMessage)
+        
     connectionSocket.close()
