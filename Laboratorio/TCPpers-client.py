ab904e16dce982b91308331dc2c94c011ffb5774
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2018-05-01 09:55:20 +0200

    Update TCPpers-client.py

--- Laboratorio/TCPpers-client.py
+++ Laboratorio/TCPpers-client.py
@@ -5,11 +5,16 @@ serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+
 while 1:
     message = raw_input('Inserire messaggio (. per terminare)')
+    
     clientSocket.send(message)
+    
     if message == '.':
         break
+        
     modifiedMessage = clientSocket.recv(1024)
     print 'Message ricevuto: ', modifiedMessage
+    
 clientSocket.close()
